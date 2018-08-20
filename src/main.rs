f2f3857ffa103927f6d567a751a3686a84cd2249
Gerrit Pape <papeg@mail.upb.de>
2018-08-20 21:08:45 +0200

    initial

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+#[macro_use]
+extern crate clap;
+
+extern crate lpr;
+
+use lpr::LprConnection;
+
+fn main() {
+	let matches = clap_app!(lpr =>
+        (version: "0.1.0")
+        (author: "Gerrit Pape <papeg@crowler.org>")
+        (about: "simple LPR client using the classic Line Printer Daemon Protocol")
+        (@arg status: -s --status "Prints the status of the queue")
+        (@arg verbose: -v -vv --verbose "Go verbose")
+        (@arg printer: +required "Address of Printer to target")
+        (@arg file: "File to print")
+    ).get_matches();
+
+	let printer = matches.value_of("printer").unwrap_or("127.0.0.1");
+
+	let mut conn = LprConnection::new(printer, matches.is_present("verbose"));
+
+	if matches.is_present("status") {
+		println!(
+			"{}",
+			match conn.status() {
+				Ok(status) => status,
+				Err(e) => e.to_string(),
+			}
+		);
+	} else if matches.is_present("file") {
+		let file = matches.value_of("file").unwrap();
+		conn.print_file(file);
+	}
+}
